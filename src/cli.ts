d211002911cf79361fafd1b94299645e7bb16143
David Sveningsson <ext@sidvind.com>
2026-07-18 01:07:11 +0200

    feat(deps): use native `parseArgs()` instead of `argparse` dependency

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-import { ArgumentParser } from "argparse";
+import { parseArgs } from "node:util";
 import { getMinPeer } from "./get-min-peer";
 
 /* eslint-disable-next-line @typescript-eslint/no-require-imports -- technical debt, should be replaced with fs.readfile */
@@ -13,24 +13,55 @@ interface Args {
 	withName?: boolean;
 }
 
+const usage = `usage: npm-min-peer [-h] [-v] [--major MAJOR] [--with-name] PACKAGE
+
+${description}
+
+positional arguments:
+  PACKAGE        package name to find version for
+
+options:
+  -h, --help     show this help message and exit
+  -v, --version  show program's version number and exit
+  --major MAJOR  constrain version to this major
+  --with-name    include package name in output`;
+
 /**
  * @internal
  */
 export async function cli(argv: string[]): Promise<number> {
-	const parser = new ArgumentParser({
-		description,
+	const { values, positionals } = parseArgs({
+		args: argv,
+		options: {
+			help: { type: "boolean", short: "h" },
+			version: { type: "boolean", short: "v" },
+			major: { type: "string" },
+			"with-name": { type: "boolean" },
+		},
+		allowPositionals: true,
 	});
 
-	parser.add_argument("-v", "--version", { action: "version", version });
-	parser.add_argument("--major", { help: "constrain version to this major" });
-	parser.add_argument("--with-name", {
-		help: "include package name in output",
-		dest: "withName",
-		action: "store_true",
-	});
-	parser.add_argument("pkgName", { help: "package name to find version for", metavar: "PACKAGE" });
+	if (values.help) {
+		console.log(usage); // eslint-disable-line no-console -- expected to log
+		return 0;
+	}
 
-	const args = parser.parse_args(argv) as Args;
+	if (values.version) {
+		console.log(version); // eslint-disable-line no-console -- expected to log
+		return 0;
+	}
+
+	const [pkgName] = positionals;
+	if (!pkgName) {
+		console.error(usage); // eslint-disable-line no-console -- expected to log
+		return 1;
+	}
+
+	const args: Args = {
+		pkgName,
+		...(values.major !== undefined && { major: values.major }),
+		...(values["with-name"] !== undefined && { withName: values["with-name"] }),
+	};
 	const result = await getMinPeer(args.pkgName, args);
 	console.log(result); // eslint-disable-line no-console -- expected to log
 	return 0;
